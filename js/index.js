cee9cd71c9c27a0571e26941bc939a1886b7084c
BillHuangg <BillHuangg@gmail.com>
2015-06-30 12:54:57 +0800

    update

--- js/index.js
+++ js/index.js
@@ -35,48 +35,47 @@ jQuery(document).ready(function ($) {
         wx.ready(function(){
             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
             console.log('ready');
-            // alert('ready');
-            // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
-            $('#contact-button').click(function() {
-                wx.checkJsApi({
-                    jsApiList: [
-                        'getNetworkType',
-                        'previewImage'
-                    ],
-                    success: function (res) {
-                        alert(JSON.stringify(res));
-                    }
-                });
+            wx.onMenuShareTimeline({
+                title: '安居客－购房十年', // 分享标题
+                link: 'http://www.miugodigital.com/apps/anjukeweb2', // 分享链接
+                imgUrl: 'http://www.miugodigital.com/apps/anjukeweb2/src/icon.jpg', // 分享图标
+                success: function () { 
+                    // 
+                },
+                cancel: function () { 
+                    //
+                }
             });
-        
-            $('#share-button').click(function () {
-                wx.onMenuShareAppMessage({
-                    title: '互联网之子',
-                    desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-                    link: 'http://movie.douban.com/subject/25785114/',
-                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-                    trigger: function (res) {
-                        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                        alert('用户点击发送给朋友');
-                    },
-                    success: function (res) {
-                        alert('已分享');
-                    },
-                    cancel: function (res) {
-                        alert('已取消');
-                    },
-                    fail: function (res) {
-                        alert(JSON.stringify(res));
-                    }
-                });
-                alert('已注册获取“发送给朋友”状态事件');
-            });
-        });
 
-        // wx.error(function(res) {
-        //     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-        //     console.log(res);
-        // });
+            wx.onMenuShareAppMessage({
+                title: '安居客－购房十年',
+                desc: '安居客现金红包！抚慰购房十年你的心！',
+                link: 'http://www.miugodigital.com/apps/anjukeweb2',
+                imgUrl: 'http://www.miugodigital.com/apps/anjukeweb2/src/icon.jpg',
+
+                trigger: function (res) {
+                    //alert('用户点击发送给朋友');
+                },
+
+                success: function (res) {
+                    //alert('已分享');
+                },
+
+                cancel: function (res) {
+                    //alert('已取消');
+                },
+
+                fail: function (res) {
+                    //alert(JSON.stringify(res));
+                }
+            });
+            
+        });
+        
+        wx.error(function(res) {
+            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+            console.log(res);
+        });
         
     }
 
